ebf2aa4ff81a33f571c60c0429c73b78c97d236d
Nelson Ramirez <xhyskoax@gmail.com>
2023-01-12 20:10:31 -0300

    trabajando con localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,5 @@
-let productos = [
-    {
-        id: "p0001",
-        nombre: "Televisor",
-        descripcion: "Es un televisor.",
-        precio: 100000,
-        stock: 10
-    },
-    {
-        id: "p0002",
-        nombre: "Radio",
-        descripcion: "Es una radio.",
-        precio: 50000,
-        stock: 5
-    },
-    {
-        id: "p0003",
-        nombre: "Notebook",
-        descripcion: "Es un Notebook.",
-        precio: 350000,
-        stock: 3
-    }
-]
-
-
-class Producto{
-    constructor(id, nombre = "", descripcion = "Sin descripción", precio = 999999, stock = 0){
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion= descripcion
-        this.precio = precio;
-        this.stock = stock;
-    }
-
-    getProducts(){
-        return productos;
-    }
-    getProduct(){
-        return productos.find(producto => producto.id == this.id);
-    }
-    deleteProduct(){
-        productos = productos.filter(producto => producto.id != this.id)
-        return productos;
-    }
-    updateProduct(){
-        let producto = productos.find(producto => producto.id ==this.id)
-        producto.nombre= this.nombre;
-        producto.descripcion= this.descripcion;
-        producto.precio = this.precio;
-        producto.stock = this.stock;
-        return producto;
-    }
-    addProduct(){
-        productos.push(
-            {
-                id: this.id,
-                nombre: this.nombre,
-                descripcion: this.descripcion,
-                precio: this.precio, 
-                stock: this.stock
-            }
-            )
-        return productos
-    }
-
-}
+import productos from "./productos.js"
+import Producto from "./Producto.js";
 
 function cargarTabla(listaProductos){
     let cuerpoTabla = document.querySelector(".section_mantenedor_productos tbody");
@@ -90,6 +26,10 @@ function buscarProducto(id){
     return producto.getProduct();
 }
 
+crud_form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+})
+
 
 //funcion que capture evento del input crud_id
 
@@ -114,7 +54,13 @@ inputId.addEventListener("change", (event) =>{
 
 
 function main(){
-    cargarTabla(productos);
+    let productosStorage = JSON.parse(localStorage.getItem("productos"));
+    if(!productosStorage){
+        productosStorage = productos;
+        localStorage.setItem("productos", JSON.stringify(productosStorage))
+    }
+
+    cargarTabla(productosStorage);
 
 }
 
